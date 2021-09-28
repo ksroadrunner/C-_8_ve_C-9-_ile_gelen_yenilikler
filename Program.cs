6c44a9a35beda524bd07802bb18901b745152b58
ksroadrunner <88201188+ksroadrunner@users.noreply.github.com>
2021-09-28 22:30:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/configure-language-version
+            var exArr = new Example.Array();
+            exArr.Run();
+
+            var exFunc = new Example.Function();
+            exFunc.Run();
+
+            var exInt = new Example.Interface();
+            exInt.Run();
+
+            var exOpt = new Example.Operators();
+            exOpt.Run();
+
+            var exSwitch = new Example.SwitchCase();
+
+            var exOth = new Example.Other();
+            exOth.Run1();
+            exOth.Run2();
+            exOth.Run3();
+            exOth.Run4();
+
+            Console.ReadKey();
+        }
+    }
+}
